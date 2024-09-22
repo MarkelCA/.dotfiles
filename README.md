ce3e4998d0c622e5945b76fb84048628e76ac976
MarkelCA <cuestaarribas.markel@proton.me>
2024-09-22 19:18:22 +0200

    Added config for nvim

---
+++ README.md
@@ -0,0 +1,11 @@
+# .dotfiles
+## Dependencies
+- [stow](https://www.gnu.org/software/stow/)
+
+## Install
+```bash
+# Add symlinks
+stow -t ~/.config config
+# Remove symlinks
+stow -t ~/.config -D config
+```
---
+++ config/nvim/init.lua
@@ -0,0 +1,54 @@
+require 'config.globals'
+require 'config.keymap'
+require 'config.dvorak'
+require 'config.opt'
+require 'config.autocmd'
+require 'config.lazy'
+
+require('lazy').setup({
+  require 'plugins.visual-multi',
+  require 'plugins.fugitive',
+  require 'plugins.easymotion',
+  require 'plugins.undotree',
+  require 'plugins.harpoon',
+  require 'plugins.neotree',
+  require 'plugins.telescope',
+  require 'plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'plugins.indent_line',
+  require 'plugins.autopairs',
+  require 'plugins.cmp',
+  require 'plugins.lsp',
+  require 'plugins.conform',
+  require 'plugins.tokyonight',
+  require 'plugins.treesitter',
+  require 'plugins.mini',
+  require 'plugins.whichkey',
+  -- require 'plugins.dap',
+  -- require 'plugins.lint',
+  { 'tpope/vim-sleuth' }, -- Detect tabstop and shiftwidth automatically
+  { 'numToStr/Comment.nvim', opts = {} },
+  { 'github/copilot.vim' },
+  -- Highlight todo, notes, etc in comments
+  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  -- Collection of various small independent plugins/modules
+}, {
+  ui = {
+    -- If you are using a Nerd Font: set icons to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})
